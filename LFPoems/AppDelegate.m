b3e24e5f3f6d903fdf5d4a5403caf7556ddeb641
agger0207 <agger0207@users.noreply.github.com>
2018-09-22 23:09:43 -0600

    Add database

--- LFPoems/AppDelegate.m
+++ LFPoems/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LFPoemsDatabaseHelper.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [self initData];
+    
     return YES;
 }
 
@@ -40,6 +44,12 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - Init Data
+
+- (void)initData {
+    [LFPoemsDatabaseHelper setupDB];
 }
 
 @end
--- LFPoems/Utils/Database/LFPoemsDatabaseHelper.m
+++ LFPoems/Utils/Database/LFPoemsDatabaseHelper.m
@@ -8,6 +8,7 @@
 
 #import "LFPoemsDatabaseHelper.h"
 #import "FMDatabaseQueue.h"
+#import "LFCommonUtility.h"
 
 NSString * const LFPoemsDBName = @"LFPoems.db";
 NSString * const LFPoemsDBFolder = @"LFPoems";
@@ -40,6 +41,46 @@ NSString * const LFPoemsSqlFile = @"LFPoems.sql";
     
     NSArray* sqls = [content componentsSeparatedByString:@";"];
     return sqls;
+}
+
+// 由于本应用采用默认数据库，所以不需要执行sql语句来展示处理.
+// 但其实收藏功能等还是需要对数据库重新组织或者更新
++ (BOOL)setupDB {
+    NSString *cacheFolder = [self cacheFolder];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:cacheFolder]) {
+        [fileManager createDirectoryAtPath:cacheFolder withIntermediateDirectories:YES attributes:nil error:NULL];
+    }
+    
+    NSString *dbFilePath = [cacheFolder stringByAppendingPathComponent:[self defaultDBFileName]];
+    BOOL isDbExist = [[NSFileManager defaultManager] fileExistsAtPath:dbFilePath];
+    BOOL isFirstInstall = !isDbExist;
+    BOOL bRet = YES;
+    if (isFirstInstall) {
+        // 获得数据库文件在工程中的路径——源路径。
+        NSString *sourcesPath = [[NSBundle mainBundle] pathForResource:@"tangshi" ofType:@"db"];
+        
+        // dbFilePath is /Users/xiangconnie/Library/Developer/CoreSimulator/Devices/1ECBA13C-08F3-45F4-8B30-02CD4089B44F/data/Containers/Data/Application/1D47B2A4-CB48-41D6-BFDC-EDEE0DCE994D/Library/Caches/LFPoems/LFPoems.db
+        NSError *error;
+        if ([[NSFileManager defaultManager] copyItemAtPath:sourcesPath toPath:dbFilePath error:&error]) {
+            NSLog(@"数据库移动成功");
+        } else {
+            NSLog(@"数据库移动失败");
+        }
+    } else {
+//        暂时并不支持升级数据库. 将来可考虑.
+    }
+    
+    FMDatabase* db = [[[self class] sharedInstance] getDatabase];
+    [db setShouldCacheStatements:NO];
+    [[[self class] sharedInstance] finish];
+    
+    if (isFirstInstall && ![self isAllowBackup]) {
+        // 缓存数据库不要备份到iCloud.
+        [LFCommonUtility addDoNotBackupAttribute:dbFilePath];
+    }
+    
+    return bRet;
 }
 
 #pragma mark - Helper Method
