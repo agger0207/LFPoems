df677dd814d5074f15dcfa013690b584b95de962
agger0207 <agger0207@users.noreply.github.com>
2018-09-23 00:07:54 -0600

    展示诗歌内容

--- LFPoems/Views/LFPoemContentCell.m
+++ LFPoems/Views/LFPoemContentCell.m
@@ -8,6 +8,10 @@
 
 #import "LFPoemContentCell.h"
 #import "Masonry.h"
+#import "LFPoem.h"
+#import "LFPoet.h"
+
+NSInteger const maxLengthOneLine = 20;
 
 @interface LFPoemContentCell ()
 
@@ -15,6 +19,7 @@
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *poetLabel;
 @property (nonatomic, strong) NSMutableArray<UILabel *> *contentLabels;
+@property (nonatomic, strong) NSArray<NSString *> *paragraphs;
 
 @end
 
@@ -38,35 +43,8 @@
         _poetLabel.textColor = [UIColor grayColor];
         _poetLabel.text = @"李白";
         
-//        NSArray *paragraphs = @[@"床前明月光，疑是地上霜。", @"举头望明月，低头思故乡。"];
-//        月下独酌\r\n\r\n李白\r\n\r\n花间一壶酒，独酌无相亲。举杯邀明月，对影成三人。月既不解饮，影徒随我身。暂伴月将影，行乐须及春。我歌月徘徊，我舞影零乱。醒时同交欢，醉后各分散。永结无情游，相期邈云汉。
-        NSArray *paragraphs = @[
-                                @"这是",
-                                @"明月，对影成三人。",
-                                @"月既不解饮，影徒随我身。",
-                                @"暂伴月将影，行乐须及春。",
-                                @"我歌月徘徊，我舞影零乱。",
-                                @"醒时同交欢，醉后各分散。",
-                                @"永结无情游，相期邈云汉。"
-                                ];
-        
-        NSInteger maxContentLen = 0;
-        for (NSString *content in paragraphs) {
-            UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
-            label.font = [UIFont systemFontOfSize:15];
-            label.textAlignment = NSTextAlignmentCenter;
-            label.text = content;
-            if (content.length > maxContentLen) {
-                _longestContentLabel = label;
-            }
-            [_contentLabels addObject:label];
-        }
-        
         [self.contentView addSubview:_titleLabel];
         [self.contentView addSubview:_poetLabel];
-        for (UILabel *label in _contentLabels) {
-            [self.contentView addSubview:label];
-        }
         [_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(self.contentView.mas_top).with.offset(10);
             make.height.mas_equalTo(32);
@@ -77,24 +55,17 @@
             make.height.mas_equalTo(30);
             make.centerX.mas_equalTo(self.contentView.mas_centerX).with.offset(0);
         }];
-        [_longestContentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-           make.centerX.mas_equalTo(self.contentView.mas_centerX).with.offset(0);
-        }];
-        UILabel *previousLabel = _poetLabel;
-        for (UILabel *label in _contentLabels) {
-            [label mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.top.equalTo(previousLabel.mas_bottom).with.offset(0);
-                make.height.mas_equalTo(24);
-                if (label != _longestContentLabel) {
-                    make.left.mas_equalTo(_longestContentLabel.mas_left).with.offset(0);
-                }
-            }];
-            previousLabel = label;
-        }
         
-        [previousLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.bottom.equalTo(self.contentView.mas_bottom).with.offset(-10);
-        }];
+        NSArray *paragraphs = @[
+                                @"这是",
+                                @"明月，对影成三人。",
+                                @"月既不解饮，影徒随我身。",
+                                @"暂伴月将影，行乐须及春。",
+                                @"我歌月徘徊，我舞影零乱。",
+                                @"醒时同交欢，醉后各分散。",
+                                @"永结无情游，相期邈云汉。"
+                                ];
+        [self showPoemContent:paragraphs];
     }
     
     return self;
@@ -111,8 +82,60 @@
     // Configure the view for the selected state
 }
 
-- (void)updateWithPoem:(LFPoem *)poem {
+#pragma mark - Show Poem
+
+- (void)showPoemContent:(NSArray<NSString *> *)paragraphs {
+    self.longestContentLabel = NULL;
+    for (UILabel *label in self.contentLabels) {
+        [label removeFromSuperview];
+    }
     
+    [self.contentLabels removeAllObjects];
+    
+    NSInteger maxContentLen = 0;
+    for (NSString *content in paragraphs) {
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+        label.font = [UIFont systemFontOfSize:15];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.text = content;
+        if (content.length > maxContentLen && content.length < maxLengthOneLine) {
+            self.longestContentLabel = label;
+        }
+        [self.contentLabels addObject:label];
+    }
+    for (UILabel *label in self.contentLabels) {
+        [self.contentView addSubview:label];
+    }
+    [self.longestContentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.contentView.mas_centerX).with.offset(0);
+    }];
+    UILabel *previousLabel = self.poetLabel;
+    for (UILabel *label in self.contentLabels) {
+        [label mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(previousLabel.mas_bottom).with.offset(0);
+            make.height.mas_equalTo(24);
+            if (label != self.longestContentLabel) {
+                make.left.mas_equalTo(self.longestContentLabel.mas_left).with.offset(0);
+            }
+        }];
+        previousLabel = label;
+    }
+    
+    [previousLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.contentView.mas_bottom).with.offset(-10);
+    }];
+}
+
+- (void)updateWithPoem:(LFPoem *)poem {
+    self.titleLabel.text = poem.title;
+    self.poetLabel.text = poem.poet.name;
+    NSLog(@"content: %@", poem.content);
+    self.paragraphs = [poem.content componentsSeparatedByString: @"\n"];
+    NSLog(@"paragraphs: %lu", self.paragraphs.count);
+    for (NSString *paragraph in self.paragraphs) {
+        NSLog(@"paragraph: %@", paragraph);
+    }
+    [self showPoemContent:self.paragraphs];
 }
 
 @end
